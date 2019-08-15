48af4dee0b1cc0ec853591923997ffd4425286a1
Trinity Siemer <tsiemer@gmail.com>
2019-08-15 15:49:11 -0500

    Changed the route for the pages

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -7,7 +7,7 @@ exports.linkResolver = function linkResolver(doc) {
   if (doc.type === 'post') {
     return '/blog/' + doc.uid;
   } else if (doc.type === 'page'){
-    return '/pages/' + doc.uid;
+    return doc.uid;
   }
 
   // Homepage route fallback
