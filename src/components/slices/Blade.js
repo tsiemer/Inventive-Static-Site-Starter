738e657f1f2e7a2338c4911f9257dc38f288aa23
Trinity Siemer <tsiemer@gmail.com>
2019-08-15 18:29:46 -0500

    Fixed the blade content

--- src/components/slices/Blade.js
+++ src/components/slices/Blade.js
@@ -4,15 +4,15 @@ import { linkResolver } from '../../utils/linkResolver'
 import htmlSerializer from '../../utils/htmlSerializer'
 
 export default ({ slice }) =>
-  <div className="blade">
+  <div className="blade container">
     <div className="image">
-      <img src={`${slice.primary.image}`} alt={"Something Awesome sorry for no better definition."}/>
+      <img src={`${slice.primary.featured_image.url}`} alt={"Something Awesome sorry for not having a better description."}/>
     </div>
     <div className="content-container">
-        <h3>{ slice.primary.subtitle }</h3>
-        <h2> { slice.primary.title } </h2>
+        <h3>{ RichText.render(slice.primary.subtitle, linkResolver, htmlSerializer) }</h3>
+        <h2> { RichText.render(slice.primary.section_title, linkResolver, htmlSerializer) } </h2>
         <div>
-            { RichText.render(slice.primary.text, linkResolver, htmlSerializer) }
+            { RichText.render(slice.primary.content, linkResolver, htmlSerializer) }
         </div>
     </div>
   </div>
