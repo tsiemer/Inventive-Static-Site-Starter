9486bebec9055a5e5daede63c292b47bb6445bc5
Trinity Siemer <tsiemer@gmail.com>
2019-08-22 15:19:50 -0500

    Initial Commit

---
+++ IGP-Framework/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import prismicLogo from '../../../src/images/logo-prismic.svg'
+
+export default () =>
+  <footer className="container">
+    <p>
+      Made with &hearts; by Inventive
+    </p>
+  </footer>
